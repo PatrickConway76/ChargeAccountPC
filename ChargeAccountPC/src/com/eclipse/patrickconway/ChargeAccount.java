73fb642df907a7edee8c7a607198ff6c7d44d546
Patrick Conway <patriconway@my.lonestar.edu>
2017-04-24 20:21:24 -0500

    Completed project with javadocs included.

--- ChargeAccountPC/src/com/eclipse/patrickconway/ChargeAccount.java
+++ ChargeAccountPC/src/com/eclipse/patrickconway/ChargeAccount.java
@@ -1,20 +1,32 @@
 package com.eclipse.patrickconway;
 
+/**
+ * Provides a static method for checking if input is a valid account
+ * @author Patrick Conway
+ *
+ */
 public final class ChargeAccount
 {
 	private static int[] accountNumbers = {5658845, 4520125, 7895122, 8777541, 8451277, 1302850,
 									8080152, 4562555, 5552012, 5050552, 7825877, 1250255,
 									1005231, 6545231, 3852085, 7576651, 7881200, 4581002};
 	
+	/**
+	 * Checks to see if the user provided account number is in the list
+	 * @param accountNumber The user provided account number
+	 * @return True if it's in the list; false if it isn't
+	 */
 	public static boolean isValidAccount(int accountNumber)
 	{
 		int index = 0;
 		boolean accountFound = false;
 		
+		// Loop until either account is found or we reach the end of the array
 		while (!accountFound && index < accountNumbers.length)
 		{
 			if (accountNumbers[index] == accountNumber)
 			{
+				// Early exit if we've found a match
 				accountFound = true;
 			}
 			index++;
--- ChargeAccountPC/src/com/eclipse/patrickconway/ChargeAccountTester.java
+++ ChargeAccountPC/src/com/eclipse/patrickconway/ChargeAccountTester.java
@@ -1,20 +1,72 @@
 package com.eclipse.patrickconway;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ChargeAccountTester
 {
 
 	public static void main(String[] args)
 	{
-		boolean crap = ChargeAccount.isValidAccount(5658845);
-		if (crap == true)
+		boolean keepGoing = true;
+		boolean hasNumber;
+		int accountNumber = 0;
+		
+		//Asks the user for an account number, compares it, reports the result, and repeats it until user enters -1
+		while (keepGoing == true)
 		{
-			System.out.println("true");
+			// Get the account number from the user
+			accountNumber = mainMenu();
+			
+			// If user wants to exit then set the flag to exit
+			if (accountNumber == -1)
+			{
+				keepGoing = false;
+			}
+			else
+			{
+				// Check to see if it's in the list
+				hasNumber = ChargeAccount.isValidAccount(accountNumber);
+				
+				// Print the result
+				if (hasNumber == true)
+				{
+					System.out.println(accountNumber + " is in the list.");
+				}
+				else
+				{
+					System.out.println(accountNumber + " is not in the list.");
+				}
+			}
 		}
-		else
+		
+		// Exit application when done
+		System.exit(0);
+	}
+	
+	// Asks the user for an account number, returns it if it's valid input 
+	private static int mainMenu()
+	{
+		Scanner sc = new Scanner(System.in);
+		boolean isValidInput = false;
+		int accountNumber = 0;
+		
+		// Keep asking forever until user enters an int
+		while (isValidInput == false)
 		{
-			System.out.print("not true");
+			try
+			{
+				System.out.print("Please enter an account number to see if it's in the list or -1 to exit: ");
+				accountNumber = sc.nextInt();
+				isValidInput = true;
+			}
+			catch (InputMismatchException e)
+			{
+				String badInput = sc.next(); // Need to consume bad input
+				System.out.println(badInput + " is invalid input. Please try again.");
+			}
 		}
-
+		return accountNumber;
 	}
 
 }
